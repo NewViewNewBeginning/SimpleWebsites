13f384a676cb87731e2f3620b1a71854c44806f3
Michal Krawczyk <michalkrawczyk1@gmail.com>
2022-05-10 16:42:55 +0100

    script for menu open/close with changing icon

--- HairStudio/app.js
+++ HairStudio/app.js
@@ -1,7 +1,17 @@
-const menuBtn = document.querySelector('#menu-btn')
+const menuBtn = document.querySelector(".menu-btn");
+const sideNav = document.querySelector(".side-nav");
+const menuIconOpen = document.querySelector(".menu-icon-open");
+const menuIconClose = document.querySelector('.menu-icon-close')
 
-const openMenu = () => { 
-   
- }
+const openCloseMenu = () => {
+	sideNav.classList.toggle('hidden')
+    if(sideNav.classList.contains('hidden')){
+        menuIconClose.classList.add('hide-icon')
+        menuIconOpen.classList.remove('hide-icon')
+    }else{
+        menuIconOpen.classList.add('hide-icon')
+        menuIconClose.classList.remove('hide-icon')
+    }
+};
 
-menuBtn.addEventListener('click', openMenu)
+menuBtn.addEventListener("click", openCloseMenu);
